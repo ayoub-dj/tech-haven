cd6ce45fa81a527c96836e8308b27303d72d300c
ayoub <ayoubhost0@gmail.com>
2024-09-28 20:04:09 -0700

    fix some bugs

--- src/TechHaven/store/urls.py
+++ src/TechHaven/store/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+
+    path('login/',  views.login_view, name='login'),
+
+    path('register/',  views.register_view, name='register'),
+
+    path('logout/',  views.logout_view, name='logout'),
+
+    path('cart/', views.cart_view, name='cart'),
+
 ]
--- src/TechHaven/store/views.py
+++ src/TechHaven/store/views.py
@@ -90,3 +90,254 @@ def home_view(request):
     
     return render(request, 'index.html', context)
 # End Home View
+
+
+# Start Login View
+def login_view(request):
+    items_count = getCartCookie(request)['items_count']
+
+    try:
+        wishlist = WishList.objects.filter(customer_wishlist=request.user.customer)
+        wishlist_count = wishlist.count()
+    except:
+        wishlist = None
+        wishlist_count = None
+
+    # Start Start Header Handler
+    mobile_phones = header_handler()['mobile_phones']
+    mobile_phones_os = header_handler()['mobile_phones_os']
+    mobile_phones_hot_brand = header_handler()['mobile_phones_hot_brand']
+
+    computers = header_handler()['computers']
+    computers_appearance = header_handler()['computers_appearance']
+    computers_hot_brand = header_handler()['computers_hot_brand']
+
+    brands  = header_handler()['brands']
+    brands_categories = header_handler()['brands_categories']
+
+    accessories = header_handler()['accessories']
+    accessories_categories = header_handler()['accessories_categories']
+
+    electronics = header_handler()['electronics']
+    electronics_categories = header_handler()['electronics_categories']
+    # End Start Header Handler
+
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Your successful Logged in  Welcome Back :) !")
+                if request.GET.get('next'): return redirect(request.GET.get('next'))
+                else: return redirect('home')
+
+    else:  form = LoginForm()
+
+    context = {
+        'form': form,
+
+        'items_count': items_count,
+
+        'wishlist_count': wishlist_count,
+
+        # Start Header Handler
+        'mobile_phones': mobile_phones,
+        'mobile_phones_os': mobile_phones_os,
+        'mobile_phones_hot_brand': mobile_phones_hot_brand,
+        'computers': computers,
+        'computers_appearance': computers_appearance,
+        'computers_hot_brand': computers_hot_brand,
+        'brands': brands,
+        'brands_categories': brands_categories,
+        'accessories': accessories,
+        'accessories_categories': accessories_categories,
+        'electronics': electronics,
+        'electronics_categories': electronics_categories,
+        # End Header Handler
+
+    }
+    return render(request, 'login.html', context)
+# End Login View
+
+
+# Start Register View
+def register_view(request):
+    items_count = getCartCookie(request)['items_count']
+
+    try:
+        wishlist = WishList.objects.filter(customer_wishlist=request.user.customer)
+        wishlist_count = wishlist.count()
+    except:
+        wishlist = None
+        wishlist_count = None
+
+    # Start Start Header Handler
+    mobile_phones = header_handler()['mobile_phones']
+    mobile_phones_os = header_handler()['mobile_phones_os']
+    mobile_phones_hot_brand = header_handler()['mobile_phones_hot_brand']
+
+    computers = header_handler()['computers']
+    computers_appearance = header_handler()['computers_appearance']
+    computers_hot_brand = header_handler()['computers_hot_brand']
+
+    brands  = header_handler()['brands']
+    brands_categories = header_handler()['brands_categories']
+
+    accessories = header_handler()['accessories']
+    accessories_categories = header_handler()['accessories_categories']
+
+    electronics = header_handler()['electronics']
+    electronics_categories = header_handler()['electronics_categories']
+    # End Start Header Handler
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+
+            try:
+                user = User(email=email, username=username)
+                user.set_password(password)
+                
+                user.first_name = first_name
+                user.last_name = last_name
+                user.save()
+                user = authenticate(request, email=email, password=password)
+            except:
+                user = None
+
+            customer = form.save(commit=False)
+
+            if user is not None:
+                customer.user = user
+                customer.save()
+                login(request, user)
+                messages.success(request, "Your registration was successful—thank you for choosing us !")
+                return redirect('home')
+            
+    else: form = CustomerForm()
+
+    context = {
+        'form': form,
+
+        'items_count': items_count,
+        'wishlist_count': wishlist_count,
+
+        # Start Header Handler
+        'mobile_phones': mobile_phones,
+        'mobile_phones_os': mobile_phones_os,
+        'mobile_phones_hot_brand': mobile_phones_hot_brand,
+        'computers': computers,
+        'computers_appearance': computers_appearance,
+        'computers_hot_brand': computers_hot_brand,
+        'brands': brands,
+        'brands_categories': brands_categories,
+        'accessories': accessories,
+        'accessories_categories': accessories_categories,
+        'electronics': electronics,
+        'electronics_categories': electronics_categories,
+        # End Header Handler
+
+    }
+    return render(request, 'register.html', context)
+# End Register View
+
+
+# Start Logout View
+@login_required(login_url='login')
+def logout_view(request):
+    logout(request)
+
+    return redirect('login')
+# End Logout View
+
+# Start Cart View
+def cart_view(request):
+    if request.method == 'POST':
+        form = CouponForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data.get('code')
+            request.session['code'] = code
+    else:
+        form = CouponForm()
+
+
+    try:
+        wishlist = WishList.objects.filter(customer_wishlist=request.user.customer)
+        wishlist_count = wishlist.count()
+    except:
+        wishlist = None
+        wishlist_count = 0
+
+    code = request.session.get('code', None)
+
+    if request.session.get('code'):
+        coupon = Coupon.objects.get(code=code)
+        total_price = coupon.apply_discount(getCartCookie(request)['total_price'])
+    else:
+        total_price = getCartCookie(request)['total_price']
+
+    cart_items = getCartCookie(request)['cart_items']
+    items_count = getCartCookie(request)['items_count']
+
+    # Start Start Header Handler
+    mobile_phones = header_handler()['mobile_phones']
+    mobile_phones_os = header_handler()['mobile_phones_os']
+    mobile_phones_hot_brand = header_handler()['mobile_phones_hot_brand']
+
+    computers = header_handler()['computers']
+    computers_appearance = header_handler()['computers_appearance']
+    computers_hot_brand = header_handler()['computers_hot_brand']
+
+    brands  = header_handler()['brands']
+    brands_categories = header_handler()['brands_categories']
+
+    accessories = header_handler()['accessories']
+    accessories_categories = header_handler()['accessories_categories']
+
+    electronics = header_handler()['electronics']
+    electronics_categories = header_handler()['electronics_categories']
+    # End Start Header Handler
+    
+
+
+    context = {
+        'cart_items': cart_items,
+
+        'wishlist_count': wishlist_count,
+
+        'items_count': items_count,
+        
+        'total_price': total_price,
+
+        'form': form,
+
+        'code': code,
+
+        # Start Header Handler
+        'mobile_phones': mobile_phones,
+        'mobile_phones_os': mobile_phones_os,
+        'mobile_phones_hot_brand': mobile_phones_hot_brand,
+        'computers': computers,
+        'computers_appearance': computers_appearance,
+        'computers_hot_brand': computers_hot_brand,
+        'brands': brands,
+        'brands_categories': brands_categories,
+        'accessories': accessories,
+        'accessories_categories': accessories_categories,
+        'electronics': electronics,
+        'electronics_categories': electronics_categories,
+        # End Header Handler
+    }
+
+    return render(request, 'cart.html', context)
+# End Cart View
+
