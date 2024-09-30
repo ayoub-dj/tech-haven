59837ef1be3d085ef9c80ca7351c25feaa664267
ayoub <ayoubhost0@gmail.com>
2024-09-30 10:36:19 -0700

    fix some bugs

--- src/TechHaven/TechHaven/urls.py
+++ src/TechHaven/TechHaven/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
